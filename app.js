53ac85fb0cd0e2fecc48e80c545073fba8a6eef7
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-11-26 11:00:23 +0530

    fix

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 // Create a text input. Allow users to enter characters in it. Show the count of characters you enter just below the input element. If the user enters more than 10 characters, change the color of the count to red.
 
 const input = document.querySelector("#text-input")
-const showOutput = document.querySelector("#output")
-// var counter = 0;
+const counter = document.querySelector("#char-count")
 
+// var charCounter = Number(counter.innerText);
 
-// console.log(inputValue);
-// console.log(inputLength);
+input.addEventListener("input", () => {
 
-input.addEventListener("change", () => {
-
-    var inputValue = input.value;
-    var inputLength = inputValue.length;
-    var counter = Number(showOutput.innerText);
-    showOutput.innerText = counter-inputLength;
-    if(inputLength > 10)
+    let inputValue = input.value.length;
+    counter.innerText = inputValue;
+    
+    if(inputValue < 10)
     {
-        input.style.color = "red";        
+        input.style.backgroundColor = "white"; 
+        counter.style.color = "black";      
+    }
+    else {
+        input.style.backgroundColor = "Red"; 
+        counter.style.color = "red";
     }
     
 })
